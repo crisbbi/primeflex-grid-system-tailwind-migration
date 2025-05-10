fd17679ef5426f6822b3cf69d4eccf730bdab445
Christian Biasi <christianbiasi@gmx.de>
2025-05-11 00:34:18 +0200

    added parameterized tests for different cases

--- utils.test.ts
+++ utils.test.ts
@@ -2,10 +2,38 @@ import { expect, describe, it } from "vitest";
 import { replaceClasses } from "./utils";
 
 describe("The CSS class migration to test", () => {
-    describe("The grid CSS class", () => {
-        it(`should be replaced with the CSS classes "flex flex-wrap -mx-2 -mt-2"`, () => {
-            const textInput = "grid";
-            expect(replaceClasses(textInput)).toBe("flex flex-wrap -mx-2 -mt-2");
+    const cssStringTestCases: { cssString: string; expectedReplacement: string }[] = [
+        { cssString: "grid", expectedReplacement: "flex flex-wrap -mx-2 -mt-2" },
+        { cssString: " grid", expectedReplacement: " flex flex-wrap -mx-2 -mt-2" },
+        { cssString: " grid ", expectedReplacement: " flex flex-wrap -mx-2 -mt-2 " },
+        { cssString: "grid ", expectedReplacement: "flex flex-wrap -mx-2 -mt-2 " },
+        { cssString: "grid grid-nogutter", expectedReplacement: "flex flex-wrap -mx-2 -mt-2 mx-0 mt-0" },
+        { cssString: " grid grid-nogutter", expectedReplacement: " flex flex-wrap -mx-2 -mt-2 mx-0 mt-0" },
+        { cssString: "grid-nogutter grid", expectedReplacement: "mx-0 mt-0 flex flex-wrap -mx-2 -mt-2" },
+        { cssString: "col", expectedReplacement: "grow basis-0 p-2" },
+        { cssString: " col", expectedReplacement: " grow basis-0 p-2" },
+        { cssString: "col ", expectedReplacement: "grow basis-0 p-2 " },
+        { cssString: " col ", expectedReplacement: " grow basis-0 p-2 " },
+        { cssString: "col col-1", expectedReplacement: "grow basis-0 p-2 grow-0 shrink-0 p-2 w-1/12" },
+        { cssString: "sm:col-1", expectedReplacement: "sm:grow-0 sm:shrink-0 sm:p-2 sm:w-1/12" },
+        {
+            cssString: "sm:col-1 md:col-6",
+            expectedReplacement: "sm:grow-0 sm:shrink-0 sm:p-2 sm:w-1/12 md:grow-0 md:shrink-0 md:p-2 md:w-1/2",
+        },
+        {
+            cssString: "sm:col-1 md:col-6 lg:col-8",
+            expectedReplacement:
+                "sm:grow-0 sm:shrink-0 sm:p-2 sm:w-1/12 md:grow-0 md:shrink-0 md:p-2 md:w-6/12 lg:grow-0 lg:shrink-0 lg:p-2 lg:w-8/12",
+        },
+        { cssString: "col-fixed", expectedReplacement: "grow-0 shrink-0 p-2" },
+        {
+            cssString: "sm:col-fixed md:col-12",
+            expectedReplacement: "sm:grow-0 sm:shrink-0 sm:p-2 md:grow-0 md:shrink-0 md:p-2 md:w-full",
+        },
+    ];
+    cssStringTestCases.forEach(({ cssString, expectedReplacement }) => {
+        it(`should replace "${cssString}" with "${expectedReplacement}"`, () => {
+            expect(replaceClasses(cssString)).toBe(expectedReplacement);
         });
     });
 });
