4d35b90900ddcfb754bb4e1db79d461b03f55c05
Christian Biasi <christianbiasi@gmx.de>
2025-05-10 20:41:10 +0200

    create classMap object + replace method for css classes

--- utils.test.ts
+++ utils.test.ts
@@ -4,11 +4,18 @@ describe("The CSS class migration to test", () => {
     describe("The grid CSS class", () => {
         it(`should be replaced with the CSS classes "flex flex-wrap -mx-2 -mt-2"`, () => {
             const textInput = "grid";
-            expect(convertPf2Tw(textInput)).toBe("flex flex-wrap -mx-2 -mt-2");
+            expect(replaceClasses(textInput)).toBe("flex flex-wrap -mx-2 -mt-2");
         });
     });
 });
 
-function convertPf2Tw(textInput: string) {
-    return textInput.replace(/grid/g, "flex flex-wrap -mx-2 -mt-2");
+const classMap = {
+    grid: "flex flex-wrap -mx-2 -mt-2",
+    col: "flex flex-col",
+    "col-fixed": "flex-shrink-0",
+} as const;
+
+function replaceClasses(input: string) {
+    const regex = new RegExp(`\\b(${Object.keys(classMap).join("|")})\\b`, "g");
+    return input.replace(regex, match => classMap[match]);
 }
