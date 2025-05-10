1f875a830394c5f1875030022532e85c25cde0e0
Christian Biasi <christianbiasi@gmx.de>
2025-05-10 19:51:54 +0200

    test lint-staged

--- utils.test.ts
+++ utils.test.ts
@@ -3,7 +3,7 @@ import { expect, describe, it } from "vitest";
 describe("The CSS class migration to test", () => {
   describe("The grid CSS class", () => {
     it(`should be replaced with the CSS classes "flex flex-wrap -mx-2 -mt-2"`, () => {
-        const textInput = "grid";
+        const textInput = "grid"
         expect(convertPf2Tw(textInput)).toBe("flex flex-wrap -mx-2 -mt-2");
     })
 
