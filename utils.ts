a94a145bb48aca5ad3875d496caefc9e0ba03077
Christian Biasi <christianbiasi@gmx.de>
2025-05-11 00:36:27 +0200

    extended map of css classes, adjusted replacement logic for "grid"/"grid-"

--- utils.ts
+++ utils.ts
@@ -1,10 +1,28 @@
 export const classMap = {
+    "grid-nogutter": "mx-0 mt-0",
     grid: "flex flex-wrap -mx-2 -mt-2",
-    col: "flex flex-col",
-    "col-fixed": "flex-shrink-0",
+    "col-fixed": "grow-0 shrink-0 p-2",
+    "col-1": "grow-0 shrink-0 p-2 w-1/12",
+    "col-2": "grow-0 shrink-0 p-2 w-2/12",
+    "col-3": "grow-0 shrink-0 p-2 w-3/12",
+    "col-4": "grow-0 shrink-0 p-2 w-4/12",
+    "col-5": "grow-0 shrink-0 p-2 w-5/12",
+    "col-6": "grow-0 shrink-0 p-2 w-6/12",
+    "col-7": "grow-0 shrink-0 p-2 w-7/12",
+    "col-8": "grow-0 shrink-0 p-2 w-8/12",
+    "col-9": "grow-0 shrink-0 p-2 w-9/12",
+    "col-10": "grow-0 shrink-0 p-2 w-10/12",
+    "col-11": "grow-0 shrink-0 p-2 w-11/12",
+    "col-12": "grow-0 shrink-0 p-2 w-full",
+    col: "grow basis-0 p-2",
 } as const;
 
 export const replaceClasses = (input: string) => {
     const regex = new RegExp(`\\b(${Object.keys(classMap).join("|")})\\b`, "g");
-    return input.replace(regex, match => classMap[match]);
+    return input.replace(regex, match => {
+        if (match.startsWith("grid-")) {
+            return classMap["grid-nogutter"];
+        }
+        return classMap[match];
+    });
 };
