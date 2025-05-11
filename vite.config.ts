abee2c35e96ec638e4819f58becb28af184a03ab
Christian Biasi <christianbiasi@gmx.de>
2025-05-11 12:01:53 +0200

    build project with vite and run with node

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
     test: {
         environment: "node",
     },
-  build: {
-    outDir: "./dist",
-  },
+    build: {
+        lib: {
+            entry: resolve(__dirname, "index.ts"),
+            formats: ["es"],
+            fileName: "index",
+        },
+        rollupOptions: {
+            external: ["glob", "node:fs", "node:readline"],
+            output: {
+                dir: "dist",
+            },
+        },
+        outDir: "dist",
+        emptyOutDir: true,
+    },
+    resolve: {
+        alias: {
+            "./utils": resolve(__dirname, "utils.ts"),
+        },
+    },
 });
